053a9f954dd94f018ad551923c12a52fec89c901
Yohan Vamour <yohan.vamour@gmail.com>
2019-10-11 18:54:05 +0200

    Load articles in UI

---
+++ GameDesignFakeNews/Assets/Script/Articles/ArticleMapper.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArticleMapper : MonoBehaviour
+{
+    [SerializeField] Text title;
+    [SerializeField] Text text;
+    public void MapArticle(Article article)
+    {
+        title.text = article.title;
+        text.text = article.text;
+    }
+}
--- GameDesignFakeNews/Assets/Script/Articles/ArticlesHandler.cs
+++ GameDesignFakeNews/Assets/Script/Articles/ArticlesHandler.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class ArticlesHandler : MonoSingleton<ArticlesHandler>
 {
+    [SerializeField] List<GameObject> articlesUI;
     public List<Article> articles = new List<Article>();
 
     public void AddArticle(Article article)
@@ -18,8 +19,14 @@ public class ArticlesHandler : MonoSingleton<ArticlesHandler>
 
     public void LoadArticles(string context)
     {
+        int index = 0;
         foreach (Article article in ArticlesLoader.Instance.LoadArticles(context))
+        {
             AddArticle(article);
+            if (index < articlesUI.Count)
+                articlesUI[index].GetComponent<ArticleMapper>().MapArticle(article);
+            index++;
+        }
     }
 
     public void ClearArticles()
