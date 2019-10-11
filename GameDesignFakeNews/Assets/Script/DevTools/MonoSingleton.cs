877265b778b75c4985d216a996362d4acab29090
Maxence <maxence.delacroix@epitech.eu>
2019-10-11 14:12:15 +0200

    adding dynamic loading of articles

---
+++ GameDesignFakeNews/Assets/Script/DevTools/MonoSingleton.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class MonoSingleton<TClass> : MonoBehaviour where TClass : MonoBehaviour
+{
+    #region Instance Handling
+    private static MonoSingleton<TClass> _instance = null;
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+            _instance = this;
+        else
+            throw new Exception("There is already an singleton instance of" + GetType().Name);
+    }
+
+    public static TClass Instance
+    {
+        get
+        {
+            if (_instance != null)
+                return _instance as TClass;
+            else
+                throw new Exception("There is no singleton instance of" + typeof(TClass).Name);
+        }
+    }
+
+    public static bool HasInstance
+    {
+        get { return _instance != null; }
+    }
+    #endregion
+}
---
+++ GameDesignFakeNews/Assets/Script/Game/DayHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayHandler : MonoSingleton<DayHandler>
+{
+    [SerializeField] private int totalDays = 1;
+    [SerializeField] private int dayIndex = 0;
+    private int endDayIndex = 0;
+
+    private void Start()
+    {
+        StartDay();
+    }
+
+    public void StartDay()
+    {
+        string context = (++dayIndex <= totalDays) ? ("Day" + dayIndex) : ("EndGame");
+        ArticlesHandler.Instance.LoadArticles(context);
+    }
+
+    public void EndDay()
+    {
+        ArticlesHandler.Instance.ClearArticles();
+        StartDay();
+    }
+}
